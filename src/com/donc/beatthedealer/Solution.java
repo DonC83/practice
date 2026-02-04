e64b53134cd7a93cafac3fec74148683eff12d2b
Donovan <594160+DonC83@users.noreply.github.com>
2026-02-04 17:25:59 +1100

    Initial commit

---
+++ src/com/donc/beatthedealer/Solution.java
@@ -0,0 +1,17 @@
+package com.donc.beatthedealer;
+
+public class Solution {
+
+
+
+  void deal() {
+
+  }
+
+  void reset(List<Player> players)
+
+  public static void main(String[] args) {
+
+  }
+
+}
